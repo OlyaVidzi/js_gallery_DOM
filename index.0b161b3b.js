67671279a0ef4668e8b17be3768050a3d530c384
Olya <olyavidzi97@gmail.com>
2024-07-19 15:47:18 +0300

    make build

---
+++ index.0b161b3b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){var r=e.target.closest("a");if(r){var a=r.href;t.src=a,e.preventDefault()}});
+//# sourceMappingURL=index.0b161b3b.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const list = document.querySelector(".gallery__list");
+const largeImg = document.getElementById("largeImg");
+list.addEventListener("click", (e)=>{
+    const link = e.target.closest("a");
+    if (link) {
+        const imgUrl = link.href;
+        largeImg.src = imgUrl;
+        e.preventDefault();
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
